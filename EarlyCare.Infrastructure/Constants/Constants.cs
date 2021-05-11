4ba7b866cd7babf550a41796303360d6acb99452
Umesh Tripathi <umesh.tripathi83@gmail.com>
2021-05-11 06:05:30 +0000

    Constants.cs edited online with Bitbucket

--- EarlyCare.Infrastructure/Constants/Constants.cs
+++ EarlyCare.Infrastructure/Constants/Constants.cs
@@ -12,18 +12,18 @@
         public static string OxigenBed = "Oxigen Bed";
         public static string VentilatorBeds = "VentilatorBeds";
 
-        public const string NewUserEmailBody = "New account has beed created.";
+        public const string NewUserEmailBody = "Welcome to Earlycare. Your account has been created.";
         public const string NewUserEmailSubject = "Welcome to Earlycare";
-        public const string PlasmaDetailsUpdatedEmailBody = "Your plasma details approved";
-        public const string PlasmaDetailsUpdatedEmailSubject = "Plasma details has beed approved";
-        public const string OxygenDetailsUpdatedEmailBody = "Your oxygen details approved";
-        public const string OxygenUpdatedEmailSubject = "Oxygen details has beed approved";
-        public const string FoodDetailsUpdatedEmailBody = "Your food details approved";
-        public const string FoodDetailsUpdatedEmailSubject = "Oxygen details has beed approved";
-        public const string VolunteerApprovedEmailSubject = "Volunteer has been approved.";
-        public const string VolunteerApprovedEmailBody = "Volunteer {0} has been approved by {1}";
-        public const string VolunteerUnapprovedEmailSubject = "Volunteer has been approved.";
-        public const string VolunteerUnapprovedEmailBody = "Volunteer {0} has been unapproved by {1}";
+        public const string PlasmaDetailsUpdatedEmailBody = "Thanks {0} for registering with us as a plasma donor. Your details have been verified by {1}. It will be shown to users.";
+        public const string PlasmaDetailsUpdatedEmailSubject = "Earlycare - Plasma details verified";
+        public const string OxygenDetailsUpdatedEmailBody = "Thanks {0} for registering with us as an oxygen supplier. Your details have been verified by {1}. It will be shown to users.";
+        public const string OxygenUpdatedEmailSubject = "Earlycare - Oxygen details verified";
+        public const string FoodDetailsUpdatedEmailBody = "Thanks {0} for registering with us as a food/tiffin supplier. Your details have been verified ny {1}. It will be shown to users.";
+        public const string FoodDetailsUpdatedEmailSubject = "Earlycare - Food/tiffin supplier details verified";
+        public const string VolunteerApprovedEmailSubject = "Earlycare - Volunteer approved";
+        public const string VolunteerApprovedEmailBody = "Thanks {0} for registering with us as a volunteer. Your registration has been approved by {1}. As a volunteer you will be able to validate/verify service providers. We show only verified suppliers in our app. So please verify a service provider before marking it as verified in the app.";
+        public const string VolunteerUnapprovedEmailSubject = "Earlycare - Volunteer has been unapproved.";
+        public const string VolunteerUnapprovedEmailBody = "Thanks {0} for registering with us as a volunteer. Your registration has been rejected by {1}. Please contact us if this was a mistake.";
     }
 
     public static class GoogleSheetNameConstants
